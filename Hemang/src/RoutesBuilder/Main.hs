c08ef1da413a2b7af0cb23cab755e56d71fef88f
Hemang-2004 <Hemang.Seth@iiitb.ac.in>
2025-04-20 13:44:59 +0530

    Added the cabal configuration and integrated with the app.cabal

--- Hemang/src/RoutesBuilder/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import RouteGen (writeModule)
-import RouteRegistry (routeRegistry)
-
-main :: IO ()
-main = do
-  let outDir = "../../generated/routes"
-  mapM_ (writeModule outDir) routeRegistry
