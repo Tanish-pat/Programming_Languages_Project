5232d070fbb884d2d66971d31e8286da385bac34
Hemang-2004 <Hemang.Seth@iiitb.ac.in>
2025-04-20 11:58:13 +0530

    Cabal project

--- generated/models/ProductCategory.hs
+++
@@ -1,9 +0,0 @@
-module ProductCategory where
-
-import Data.Typeable
-import Prelude hiding (id)
-
-data ProductCategory = ProductCategory {
-  productCategoryProductId :: String,
-  productCategoryCategoryId :: Int
-} deriving (Show, Eq, Typeable)
