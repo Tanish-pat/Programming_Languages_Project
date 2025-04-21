9e212ce3f79d779d96d4701016a2a082a287544d
Hemang-2004 <Hemang.Seth@iiitb.ac.in>
2025-04-21 19:48:42 +0530

    Added all the GET routes

--- app/Server.hs
+++ app/Server.hs
@@ -9,7 +9,7 @@ import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import Data.Text.Lazy (Text, pack)
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text as T
-import Database.SQLite.Simple (open, query_, SQLData(..), Connection)
+import Database.SQLite.Simple (open, query_, query, SQLData(..), Connection, Only(..))
 
 sqlDataToText :: SQLData -> Text
 sqlDataToText (SQLInteger i) = pack (show i)
@@ -37,3 +37,96 @@ main = do
             rows <- liftIO (query_ conn "SELECT * FROM Customer" :: IO [[SQLData]])
             let textRows = map (map sqlDataToText) rows
             json textRows
+
+        get "/customer/getById/:id" $ do
+            id <- param "id"
+            rows <- liftIO (query conn "SELECT * FROM Customer WHERE id = ?" (Only (read id :: Int)) :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/product/getAll" $ do
+            rows <- liftIO (query_ conn "SELECT * FROM Product" :: IO [[SQLData]])
+            let textRows = map (map sqlDataToText) rows
+            json textRows
+
+        get "/product/getBySku/:sku" $ do
+            sku <- param "sku"
+            rows <- liftIO (query conn "SELECT * FROM Product WHERE sku = ?" (Only (TL.toStrict sku)) :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/review/getAll" $ do
+            rows <- liftIO (query_ conn "SELECT * FROM Review" :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/review/getById/:reviewId" $ do
+            reviewId <- param "reviewId"
+            rows <- liftIO (query conn "SELECT * FROM Review WHERE reviewId = ?" (Only (read reviewId :: Int)) :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/review/product/:productId" $ do
+            productId <- param "productId"
+            rows <- liftIO (query conn "SELECT * FROM Review WHERE productId = ?" (Only (read productId :: Int)) :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/address/getAll" $ do
+            rows <- liftIO (query_ conn "SELECT * FROM Address" :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/address/getById/:addressId" $ do
+            addressId <- param "addressId"
+            rows <- liftIO (query conn "SELECT * FROM Address WHERE addressId = ?" (Only (read addressId :: Int)) :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/address/byCustomer/:customerId" $ do
+            customerId <- param "customerId"
+            rows <- liftIO (query conn "SELECT * FROM Address WHERE customerId = ?" (Only (read customerId :: Int)) :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/inventory/getAll" $ do
+            rows <- liftIO (query_ conn "SELECT * FROM Inventory" :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/inventory/getByProduct/:productId" $ do
+            productId <- param "productId"
+            rows <- liftIO (query conn "SELECT * FROM Inventory WHERE productId = ?" (Only (read productId :: Int)) :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/payment/getAll" $ do
+            rows <- liftIO (query_ conn "SELECT * FROM Payment" :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/payment/getById/:paymentId" $ do
+            paymentId <- param "paymentId"
+            rows <- liftIO (query conn "SELECT * FROM Payment WHERE paymentId = ?" (Only (read paymentId :: Int)) :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/coupon/getAll" $ do
+            rows <- liftIO (query_ conn "SELECT * FROM Coupon" :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/coupon/getByCode/:couponCode" $ do
+            couponCode <- param "couponCode"
+            rows <- liftIO (query conn "SELECT * FROM Coupon WHERE couponCode = ?" (Only (TL.toStrict couponCode)) :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/category/getAll" $ do
+            rows <- liftIO (query_ conn "SELECT * FROM Category" :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/category/getById/:categoryId" $ do
+            categoryId <- param "categoryId"
+            rows <- liftIO (query conn "SELECT * FROM Category WHERE categoryId = ?" (Only (read categoryId :: Int)) :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/productcategory/getAll" $ do
+            rows <- liftIO (query_ conn "SELECT * FROM ProductCategory" :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/productcategory/byProduct/:productId" $ do
+            productId <- param "productId"
+            rows <- liftIO (query conn "SELECT * FROM ProductCategory WHERE productId = ?" (Only (read productId :: Int)) :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
+
+        get "/productcategory/byCategory/:categoryId" $ do
+            categoryId <- param "categoryId"
+            rows <- liftIO (query conn "SELECT * FROM ProductCategory WHERE categoryId = ?" (Only (read categoryId :: Int)) :: IO [[SQLData]])
+            json (map (map sqlDataToText) rows)
